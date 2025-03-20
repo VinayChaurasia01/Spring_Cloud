c7ec23aaaddcb2ac10e332a31b115b1f4fbe611d
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-03-20 12:45:59 +0530

    Vinay Add . Quiz app using Microservices

--- Microservices/api-gateway/src/main/java/com/vinay/apigateway/ApiGatewayApplication.java
+++ Microservices/api-gateway/src/main/java/com/vinay/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.anand.apigateway;
+package com.vinay.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Microservices/api-gateway/src/test/java/com/vinay/apigateway/ApiGatewayApplicationTests.java
+++ Microservices/api-gateway/src/test/java/com/vinay/apigateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.anand.apigateway;
+package com.vinay.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
